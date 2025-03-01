481e65316cb4c16cc0e640f7aad6dd0f034d7895
emahji2683 <qurrrr31555@gmail.com>
2025-03-01 19:54:31 +0900

    Add learning records for 2025-03-01

---
+++ 20250226/1
@@ -0,0 +1,1 @@
+COPY museums_json_expand TO '/Users/a81906/TIL/20250226/output.csv' WITH CSV DELIMITER ',';
--- 20250226/table_making_museums.sql
+++ 20250226/table_making_museums.sql
@@ -3,7 +3,7 @@ CREATE TABLE :table_name (
     name VARCHAR(255),
     location VARCHAR(255),
     business_hours VARCHAR(255),
-    admission_fees VARCHAR(255),
+    admission_fees VARCHAR(500),
     website_url VARCHAR(255),
     phone_number VARCHAR(20),
     area VARCHAR(100)
